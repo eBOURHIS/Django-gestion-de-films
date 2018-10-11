c862074986208a816ada55e86231e85e8aeda0df
Damien <damien.pitache@orange.fr>
2018-10-11 09:51:38 +0200

    update template

--- Films/views.py
+++ Films/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from Films.models import Movie
 
 # Create your views here.
+
+def ListFilms(request):
+    objets = Movie.object.all().order_By('title')
+    return render(request,' ListFilms.html',{'objets':objets})
