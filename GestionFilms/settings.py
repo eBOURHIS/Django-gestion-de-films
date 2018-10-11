fe7b7443e3db826e3d2a0aa31a1c33dbc0664b11
Erwan Bourhis <bourhis@info23-12.iut45.univ-orleans.fr>
2018-10-11 09:13:44 +0200

    commit avant merge

--- GestionFilms/settings.py
+++ GestionFilms/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #Import des sessions
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
 ROOT_URLCONF = 'GestionFilms.urls'
