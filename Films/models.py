931300154a9ab4e0803eb117d148212cfaad3e1b
Erwan Bourhis <bourhis@info23-12.iut45.univ-orleans.fr>
2018-10-11 08:57:12 +0200

    Ajout du modèle

--- Films/models.py
+++ Films/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    score = models.FloatField(max_length=20)
+    realease_date = models.DateField()
+    realisator = models.OneToOneField('Realisator',on_delete=False)
+    actors = models.ForeignKey('Actor', on_delete=False)
+    genre = models.CharField(max_length=50)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Actor(models.Model):
+    firstname = models.CharField(max_length=50)
+
+class Realisator(models.Model):
+    firstname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+class Comment(models.Model):
+    user = models.CharField(max_length=50)
+    text = models.TextField(max_length=1000)
+    score = models.FloatField(max_length=50)
+    user = models.OneToOneField('User', on_delete=False)
+
+class User(models.Model):
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    admin = models.BooleanField(default=False)
