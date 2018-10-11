b0bc88d846c1cfbbe235791bdb59084a335cd27e
Nicolas Lemoine <lemoine@info23-13.iut45.univ-orleans.fr>
2018-10-11 09:27:28 +0200

    Modele

--- Films/admin.py
+++ Films/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from Films.models import Movie, Genre, Actor, Realisator, Comment, User
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'score', 'realease_date', 'realisator', 'actors', 'genre', 'comments')
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('firstname')
+
+class RealisatorAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'name')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'score', )
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Realisator, RealisatorAdmin)
+admin.site.register(Comment)
+admin.site.register(User)
