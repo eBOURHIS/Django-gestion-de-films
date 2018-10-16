7463aa2733414aa520636441fc800ae4086cda2a
Erwan Bourhis <bourhis@info21-10.iut45.univ-orleans.fr>
2018-10-16 11:38:07 +0200

    correction du modèle

--- Films/admin.py
+++ Films/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from Films.models import Movie, Genre, Actor, Realisator, Comment, User
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'score', 'realease_date', 'realisator', 'actors', 'genre', 'comments')
+    list_display = ('title', 'score', 'realease_date', 'realisator', 'actors', 'genre')
 
 class ActorAdmin(admin.ModelAdmin):
     list_display = ('firstname', 'name')
@@ -16,7 +16,7 @@ class CommentAdmin(admin.ModelAdmin):
 class UserAdmin(admin.ModelAdmin):
     list_display = ('login', 'password', 'admin')
 
-admin.site.register(Movie, MovieAdmin)
+admin.site.register(Movie)#MovieAdmin
 admin.site.register(Genre)
 admin.site.register(Actor, ActorAdmin)
 admin.site.register(Realisator, RealisatorAdmin)
