b3683b57a6ce9b6d81c8548a2460c1020c06fbe5
Nicolas Lemoine <lemoine@info21-09.iut45.univ-orleans.fr>
2018-10-16 11:56:52 +0200

    Views

--- Films/views.py
+++ Films/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from Films.models import Movie
+from Films.models import Movie, User
+from django.forms import ModelForm
 
 # Create your views here.
 
 def ListFilms(request):
     objets = Movie.objects.all().order_by('title')
     return render(request,'ListFilms.html',{'objets':objets})
+
+class InscriptionForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('login', 'password')
+        labels = {
+            'pseudo': 'Pseudo',
+            'password': 'Mot de passe',
+        }
+
+def inscription(request):
+    form = InscriptionForm()
+    if request.method == "POST":
+        new_user = form.save()
