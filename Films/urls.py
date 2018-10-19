b0e489d3d7bbf5fb1ed3a634483849f61764beb7
Damien <damien.pitache@orange.fr>
2018-10-19 09:27:46 +0200

    update urls.py

--- Films/urls.py
+++ Films/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import ListFilms
 
 urlpatterns = [
-    path(r'list', views.ListFilms, name='ListFilms'),
+    path(r'', views.ListFilms, name='ListFilms'),
     path(r'update/<int:film_id>', views.UpdateFilm, name='UpdateFilm'),
     path(r'delete/<int:film_id>', views.DeleteFilm, name='DeleteFilm'),
     path(r'add', views.AddFilm, name='AddFilm'),
